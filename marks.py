b743ae1eea40a513bcfd42032d2c3877e1b72f36
usharani2007 <mejariusharani6@gamil.com>
2026-08-25 19:03:35 +0530

    flow

---
+++ marks.py
@@ -0,0 +1,21 @@
+marks=35
+
+if marks >=90:
+	print("grade A")
+elif marks >=80:
+	print("garde B")
+elif marks >=70:
+	print("grade C")
+elif marks >=60:
+	print("grade D")
+elif marks >=50:
+	print("grade e")
+elif marks >=40:
+	print("grade f")
+else:
+	print("fail")
+
+
+
+
+
